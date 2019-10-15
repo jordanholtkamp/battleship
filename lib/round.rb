78bedf59743a9ef5be2382e1144b4bbc8db5866d
Joshua O'Bannon <josho@Joshuas-MacBook-Pro.local>
2019-10-15 17:19:51 -0600

    Decouple round and runner files.  Tests for round needed

--- lib/round.rb
+++ lib/round.rb
@@ -1,43 +1,46 @@
-require './lib/computer'
-require './lib/player'
-require './lib/board'
-require './lib/ship'
-require './lib/turn'
+require_relative './turn'
 class Round
 
-  def initialize
-    @computer_board = Board.new
-    @player_board = Board.new
-
-    @computer = Computer.new(@computer_board)
-    @player = Player.new(@player_board)
-
-    @computer_cruiser = Ship.new("Cruiser", 3)
-    @computer_sub = Ship.new("Submarine", 2)
-
-    @player_cruiser = Ship.new("Cruiser", 3)
-    @player_sub = Ship.new("Submarine", 2)
-
-    start_menu_message
-  end
-
-  def start_menu_message
-     puts "Welcome to BATTLESHIP"
-     puts "Enter P to Play.  Enter Q to quit"
-     menu_loop
+  def initialize(round_array_objs)
+    @computer_board = round_array_objs[0]
+    @player_board = round_array_objs[1]
+    @computer = round_array_objs[2]
+    @player = round_array_objs[3]
+    @computer_cruiser = round_array_objs[4]
+    @computer_sub = round_array_objs[5]
+    @player_cruiser = round_array_objs[6]
+    @player_sub = round_array_objs[7]
   end
 
   def menu_loop
-      user_menu_select = gets.chomp
-
+    puts "Welcome to BATTLESHIP"
+    puts "Enter P to Play.  Enter Q to quit"
+    user_menu_select = gets.chomp
       if user_menu_select.downcase == "p"
-        start_game
+        start_game = "y"
       elsif user_menu_select.downcase == "q"
         p "Sorry - you are missing out.  Dummy :)"
+        start_game = "stop"
       else
         puts "Plese select only P or Q"
-        start_menu_message
+        start_game = "n"
       end
+  end
+
+  def start_game
+    until game_over?(@player_board) || game_over?(@computer_board)
+      take_turn
+    end
+  end
+
+  def take_turn
+    turn = Turn.new(@player_board, @computer_board)
+    turn.display_boards
+    turn.user_shot
+    turn.display_boards
+    turn.computer_shot
+    turn.display_boards
+    turn.display_results
   end
 
   def setup_computer_placement
@@ -46,18 +49,6 @@ class Round
 
     computer_coords_cruiser = @computer.random_computer_placement(@computer_cruiser)
     @computer_board.place(@computer_cruiser, computer_coords_cruiser)
-  end
-
-  def check_user_coord(user_ship)
-    good_to_go = false
-    user_ship.each do |coord|
-      if @player_board.valid_coordinate?(coord)
-        good_to_go = true
-      else
-        good_to_go = false
-      end
-    end
-    good_to_go
   end
 
   def setup_user_placement
@@ -91,20 +82,20 @@ class Round
     puts @player_board.render_board(true)
   end
 
-  def validate_user_input(ship_obj, user_ship_answer)
-    check_user_coord(user_ship_answer) && @player_board.valid_placement?(ship_obj, user_ship_answer)
+  def check_user_coord(user_ship)
+    good_to_go = false
+    user_ship.each do |coord|
+      if @player_board.valid_coordinate?(coord)
+        good_to_go = true
+      else
+        good_to_go = false
+      end
+    end
+    good_to_go
   end
 
-  def take_turn
-    turn = Turn.new(@player_board, @computer_board)
-    turn.display_boards
-    turn.user_shot
-    turn.display_boards
-    turn.computer_shot
-    turn.display_boards
-    turn.display_results
-
-    # turns << turn
+  def validate_user_input(ship_obj, user_ship_answer)
+    check_user_coord(user_ship_answer) && @player_board.valid_placement?(ship_obj, user_ship_answer)
   end
 
   def game_over?(board)
@@ -115,25 +106,11 @@ class Round
     booleans_array = cells_with_ships.map do |cell|
       cell.ship.sunk?
     end
-    booleans_array.all?(true)
+    booleans_array.all? == true
   end
 
   def stop_the_game(winner)
     p "#{winner} won."
   end
 
-  def start_game
-    setup_computer_placement
-    setup_user_placement
-
-    until game_over?(@player_board) || game_over?(@computer_board)
-      take_turn
-    end
-
-    if game_over?(@player_board)
-      stop_the_game("I")
-    elsif game_over?(@computer_board)
-      stop_the_game("You")
-    end
-  end
 end
